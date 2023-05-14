876b6b1173743ad9b6154ec0577ef83d2922c601
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-14 09:37:09 -0700

    add solution

---
+++ 0_Framework/Application/ApplicationMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Framework.Application
+{
+    public class ApplicationMessages
+    {
+        public const string DouplicatedRecord = "امکان ثبت رکورد تکراری وجود ندارد، لطفا مجددا تلاش فرمائید";
+        public const string RecordNotFound = "رکورد با اطلاعات درخواست شده یافت نشد، لطفا مجددا تلاش فرمائید";
+        public const string PasswordNotMatch = "پسورد و تکرار آن با هم مطابقت ندارند";
+        public const string WrongUserPasss = "نام کاربری یا کلمه عبور اشتباه است";
+    }
+}
---
+++ 0_Framework/Domain/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Framework.Domain
+{
+   public interface IRepository<Tkey,T> where T:class
+    {        
+        T Get(Tkey id);
+        List<T> Get();
+        void Create(T entity);
+        void SaveChanges();
+        bool Exist(Expression<Func<T, bool>> expression); 
+    }
+}
