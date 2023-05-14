80f3eadccb5cc284f2321e9760d1073cff553b1b
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-14 14:56:39 -0700

    in onion architecture domain define domain to impliment our business and the object as classes we want to have in db

---
+++ BookingManagement.Domain/SalesUnitsAgg/ISalesUnitsRepository.cs
@@ -0,0 +1,16 @@
+﻿using _0_Framework.Domain;
+using BookingManagement.Application.Contract.SalesUnits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Domain.SalesUnitsAgg
+{
+    public interface ISalesUnitsRepository: IRepository<long, SalesUnit>
+    {
+        EditSalesUnit GetDetails(long id);
+        List<SalesUnitViewModel> Search(SalesUnitSearchModel searchModel);
+    }
+}
---
+++ BookingManagement.Domain/SalesUnitsAgg/SalesUnits.cs
@@ -0,0 +1,54 @@
+﻿using _0_Framework.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Domain.SalesUnitsAgg
+{
+     public class SalesUnit: EntityBase
+    {
+        
+        public string Name { get;private set; }
+        public string Country { get; private set; }
+        public string Currency { get; private set; }
+
+        // these three property didnot want from me but
+        // are nessessary in real world 
+        //PictureAlt && PictureTitle for seo
+        //i point to them but not implimenting
+        //public string Picture { get; private set; }
+        //public string PictureAlt { get; private set; }
+        //public string PictureTitle { get; private set; }
+
+        //these three property is assign and set just for seo
+        public string Keywords { get; private set; }
+        public string MetaDescription { get; private set; }
+        public string Slug { get; private set; }
+
+        public SalesUnit(string name, string country, string currency,
+            string keywords, string metaDescription,
+            string slug)
+        {
+            Name = name;
+            Country = country;
+            Currency = currency;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+
+        public void Edit(string name, string country, string currency,
+            string keywords, string metaDescription,
+            string slug)
+        {
+            Name = name;
+            Country = country;
+            Currency = currency;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Slug = slug;
+        }
+    }
+}
