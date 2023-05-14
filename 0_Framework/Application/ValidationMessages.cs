b15566efc7ff9c3a3db49ad569c8b7a8f35cf30b
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-14 14:36:39 -0700

    modified validation messages

--- 0_Framework/Application/ValidationMessages.cs
+++ 0_Framework/Application/ValidationMessages.cs
@@ -2,9 +2,9 @@
 {
     public class ValidationMessages
     {
-        public const string IsRequired = "این مقدار نمی تواند خالی باشد";
-        public const string MaxFileSize = "فایل حجیم تر از حد مجاز است";
-        public const string InvalidFileFormat = "فرمت فایل مجاز نیست";
-        public const string MaxLenght = "مقدار وارد شده بیش از طول مجاز است";
+        public const string IsRequired = "This value cannot be empty";
+        public const string MaxFileSize = "The file is larger than allowed";
+        public const string InvalidFileFormat = "The file format is not allowed";
+        public const string MaxLenght = "The entered value exceeds the allowed length";
     }
 }
