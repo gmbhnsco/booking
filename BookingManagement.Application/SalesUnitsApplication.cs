0432a9c8340b3b005c0134d0e39e63ed13d8ddf1
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-14 15:09:54 -0700

    orchestrate between domain and data layer and prepare data for ui

---
+++ BookingManagement.Application/SalesUnitsApplication.cs
@@ -0,0 +1,65 @@
+﻿using _0_Framework.Application;
+using BookingManagement.Application.Contract.SalesUnits;
+using BookingManagement.Domain.SalesUnitsAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Application
+{
+    public class SalesUnitsApplication : ISalesUnitApplication
+    {
+        private readonly ISalesUnitsRepository _salesUnitsRepository;
+
+        public SalesUnitsApplication(ISalesUnitsRepository salesUnitsRepository)
+        {
+            _salesUnitsRepository = salesUnitsRepository;
+        }
+
+        public OperationResult Create(CreateSalesUnit command)
+        {
+            var operation = new OperationResult();
+            if (_salesUnitsRepository.Exist(x=>x.Name == command.Name))
+            {
+                operation.Failed(ApplicationMessages.DouplicatedRecord);
+            }
+            var slug = command.Slug.Slugify();
+            var salesUnit = new SalesUnit(command.Name, command.Country, command.Currency,command.Keywords,command.MetaDescription,slug);
+            _salesUnitsRepository.Create(salesUnit);
+            _salesUnitsRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public OperationResult Edit(EditSalesUnit command)
+        {
+            var operation = new OperationResult();
+            var saleUnit = _salesUnitsRepository.Get(command.Id);
+            if (saleUnit == null)
+            {
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            }
+            if (_salesUnitsRepository.Exist(x=>x.Name == command.Name && x.Id != command.Id))
+            {
+                return operation.Failed(ApplicationMessages.DouplicatedRecord);
+
+            }
+            var slug = command.Slug.Slugify();
+            saleUnit.Edit(command.Name, command.Country, command.Currency,command.Keywords,command.MetaDescription,slug);
+            _salesUnitsRepository.SaveChanges();
+            return operation.Succedded();
+        }
+
+        public EditSalesUnit GetDetails(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SalesUnitViewModel> Search(SalesUnitSearchModel searchModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
