6507f5f219d5a4f68cafde8a3e73a270025b1300
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-14 20:40:25 -0700

    introduce sql as orm and wire up the system by introduce the class and interfaces

---
+++ BookingManagement.Configuration/BookingManagementBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using BookingManagement.Application;
+using BookingManagement.Application.Contract.SalesUnits;
+using BookingManagement.Infrastructure.EFCore;
+using BookingManagement.Infrastructure.EFCore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookingManagement.Configuration
+{
+    public class BookingManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<ISalesUnitApplication, SalesUnitsApplication>();
+            services.AddTransient<SalesUnitRepository, SalesUnitRepository>();
+
+            services.AddDbContext<BookingContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
