bd534a457d2a049c8c2eb912eb3bdc92192dac71
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-15 15:30:31 -0700

    add saleUnit api controller andinject salesunitApplication file into it and use the verb in its place

---
+++ ServiceHost/Controllers/SalesUnitController.cs
@@ -0,0 +1,57 @@
+﻿using BookingManagement.Application.Contract.SalesUnits;
+using BookingManagement.Domain.SalesUnitsAgg;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ServiceHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesUnitController : ControllerBase
+    {
+        private readonly ISalesUnitApplication _salesUnitApplication;
+        public SalesUnitController(ISalesUnitApplication salesUnitApplication)
+        {
+            _salesUnitApplication = salesUnitApplication;
+        }
+        // GET: api/<SalesUnitController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SalesUnitController>/5
+        [HttpGet("{id}")]
+        public EditSalesUnit Get(int id)
+        {
+            return _salesUnitApplication.GetDetails(id);
+        }
+
+        // POST api/<SalesUnitController>
+        [HttpPost]
+        public void Post(CreateSalesUnit command)
+        {
+            var result = _salesUnitApplication.Create(command);
+            RedirectToAction("./Index", result);
+        }
+
+        // PUT api/<SalesUnitController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, EditSalesUnit command)
+        {
+            _salesUnitApplication.Edit(command);
+        }
+
+        // DELETE api/<SalesUnitController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
