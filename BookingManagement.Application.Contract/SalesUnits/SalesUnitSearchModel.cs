91d82a7f9d2849c363830fe83713c536ae3176b2
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-21 20:18:27 +0330

    create search model based on country and currency and price and shopname and ..

--- BookingManagement.Application.Contract/SalesUnits/SalesUnitSearchModel.cs
+++ BookingManagement.Application.Contract/SalesUnits/SalesUnitSearchModel.cs
@@ -8,8 +8,10 @@ namespace BookingManagement.Application.Contract.SalesUnits
 {
     public class SalesUnitSearchModel
     {
-        public string Name { get; set; }
         public string Country { get; set; }
         public string Currency { get; set; }
+        public string ShopName { get; set; }
+        public string BookingDate { get; set; }
+        public double Prices { get; set; }
     }
 }
