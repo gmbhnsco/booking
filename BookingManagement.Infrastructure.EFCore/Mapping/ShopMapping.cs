1ee2eebbedce13bc52d71c6927d352f0a5932b1a
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-21 20:34:27 +0330

    creat shop mapping using fluent api

---
+++ BookingManagement.Infrastructure.EFCore/Mapping/ShopMapping.cs
@@ -0,0 +1,29 @@
+﻿using BookingManagement.Domain.ShopsAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Infrastructure.EFCore.Mapping
+{
+    class ShopMapping : IEntityTypeConfiguration<Shops>
+    {
+        public void Configure(EntityTypeBuilder<Shops> builder)
+        {
+            builder.ToTable("Shops");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(255).IsRequired();
+
+            builder.HasMany(x => x.Bookings)
+                .WithOne(x => x.Shop)
+                .HasForeignKey(x => x.ShopId);
+
+            builder.HasOne(x => x.SalesUnit).WithMany(x => x.Shops).HasForeignKey(x => x.SalesUnitId);
+
+        }
+    }
+}
