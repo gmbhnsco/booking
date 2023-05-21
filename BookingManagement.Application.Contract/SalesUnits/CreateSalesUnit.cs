c6b32fc6464ed7b8a8c4e7a1ecb5a7623cb384ba
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-21 20:15:32 +0330

    add data anotation

--- BookingManagement.Application.Contract/SalesUnits/CreateSalesUnit.cs
+++ BookingManagement.Application.Contract/SalesUnits/CreateSalesUnit.cs
@@ -11,6 +11,8 @@ namespace BookingManagement.Application.Contract.SalesUnits
     public class CreateSalesUnit
     {
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        //is not nesessary when using web api with swagger
+       // [Display(Name = "please inster {0}")]
         public string Name { get; set; }
         [Required(ErrorMessage = ValidationMessages.IsRequired)]
         public string Country { get; set; }
