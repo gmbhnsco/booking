56d835bf4ec12838f5f7f6491b1f5bde069c70e7
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-21 20:33:33 +0330

    create mapping for booking with fluent api

---
+++ BookingManagement.Infrastructure.EFCore/Mapping/BookingMapping.cs
@@ -0,0 +1,25 @@
+﻿using BookingManagement.Domain.BookingsAgg;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Infrastructure.EFCore.Mapping
+{
+    public class BookingMapping : IEntityTypeConfiguration<Bookings>
+    {
+        public void Configure(EntityTypeBuilder<Bookings> builder)
+        {
+            builder.ToTable("Bookings");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Price).IsRequired();
+
+            builder.HasOne(x => x.Shop)
+                .WithMany(x => x.Bookings)
+                .HasForeignKey(x => x.ShopId);
+        }
+    }
+}
