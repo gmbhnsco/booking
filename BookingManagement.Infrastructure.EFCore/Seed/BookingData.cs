73f0052f0ef43424619a4cad0c5413a4470fc153
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-21 20:41:14 +0330

    generate class to contain booking data class

---
+++ BookingManagement.Infrastructure.EFCore/Seed/BookingData.cs
@@ -0,0 +1,19 @@
+﻿using BookingManagement.Domain.BookingsAgg;
+using BookingManagement.Domain.SalesUnitsAgg;
+using BookingManagement.Domain.ShopsAgg;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Infrastructure.EFCore.Seed
+{
+    public class BookingData
+    {
+        public List<SalesUnits> salesUnits { get; set; }
+        public List<Shops> shops { get; set; }
+        public List<Bookings> bookings { get; set; }
+    }
+}
