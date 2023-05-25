6a395beb64239a948a038dd3e5ec14e711bf9053
saeed <saeed@DESKTOP-O0OPJKE>
2023-05-26 00:07:54 +0330

    missing files

---
+++ BookingManagement.Domain/BookingsAgg/Bookings.cs
@@ -0,0 +1,25 @@
+﻿using _0_Framework.Domain;
+using BookingManagement.Domain.ShopsAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Domain.BookingsAgg
+{
+     public class Bookings: EntityBase
+    {       
+
+        public double Price { get; set; }
+        public string BookingDate { get; set; }
+        public long ShopId { get; set; }
+        public Shops Shop { get; set; }
+        public Bookings(double price, string bookingDate, long shopId)
+        {
+            Price = price;
+            BookingDate = bookingDate;
+            ShopId = shopId;
+        }
+    }
+}
---
+++ BookingManagement.Domain/ShopsAgg/Shops.cs
@@ -0,0 +1,27 @@
+﻿using _0_Framework.Domain;
+using BookingManagement.Domain.BookingsAgg;
+using BookingManagement.Domain.SalesUnitsAgg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingManagement.Domain.ShopsAgg
+{
+    public class Shops:EntityBase
+    {
+       
+
+        public string Name { get; set; }
+        public long SalesUnitId { get; set; }
+        public SalesUnits SalesUnit { get; set; }
+        public List<Bookings> Bookings { get; set; }
+        public Shops(string name, long salesUnitId)
+        {
+            Name = name;
+            SalesUnitId = salesUnitId;
+        }
+
+    }
+}
